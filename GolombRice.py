6d248df012d866b3e23ee20449038d1d329d5ad7
Benny Sommese <benedettosommese@gmail.com>
2020-12-15 20:26:12 +0100

    Aggiunta decompressore di Golomb Rice

--- GolombRice.py
+++ GolombRice.py
@@ -1,3 +1,27 @@
+def decompression_golomb_rice(nbin, k):
+    m = 2 ** k
+    c = 0
+    while nbin[c] =='1':
+        c = c +1
+    decode1 = c * m
+    print('La prima parte del codice equivale a ' + str(decode1))
+
+    decode2 = 0
+    esponente = 0
+    c2=len(nbin)-1
+    while c2 > c:
+        if nbin[c2] == '1':
+            decode2 = decode2 + 2**esponente
+        esponente = esponente + 1
+        c2 = c2 - 1
+
+
+    print('La seconda parte del codice equivale a ' + str(decode2))
+
+    decode = decode1 + decode2
+    print("Il valore decompressato equivale a " + str(decode))
+    return decode
+
 def golomb_rice(n, k):
     m = 2 ** k
     print("Il valore m equivale a: ")
@@ -39,7 +63,7 @@ def golomb_rice(n, k):
     return code1
 
 
-p = 220
+p = 180
 l = 120
 h = 150
 k = 4
@@ -51,3 +75,13 @@ elif p > h:
 
 print("La codifica completa equivale a: ")
 print(code)
+
+
+if code[0] == '1':
+    decode = decompression_golomb_rice(code[2:],k)
+    if code[1] == '0':
+        decode = l - (decode + 1)
+        print("Il valore originale del pixel corrisponde a " + str(decode))
+    elif code[0] == '1':
+        decode = h + (decode + 1)
+        print("Il valore originale del pixel corrisponde a " + str(decode))
