813b68107bc0a77701b96b48e26a6edd95001d2a
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-12-15 22:42:43 +0100

    Aggiunta della codifica e decodifica di AdjustedBinaryCode.py.

---
+++ AdjustedBinaryCode.py
@@ -0,0 +1,135 @@
+import math
+
+def dec_bin(n,m):
+
+    b = ""
+    while n>0:
+
+        if n%2==0:
+
+            b="0"+b
+
+        else:
+
+            b="1"+b
+
+        n=int(n/2)  #casting in intero
+
+    while len(b)<m:
+        b= "0"+b
+
+    return b
+
+def adjusted_binary_codeCod(H,L,P):
+
+    delta = H - L
+    k = math.log(delta + 1, 2)
+    potenza_di_due = k.is_integer()
+    k = int(k)
+    a = (2**(int(k)+1))-(delta + 1)
+    b = 2*(delta + 1 - 2**int(k))
+
+    A = []
+
+    if potenza_di_due == True:
+        start = L
+        for i in range (0,delta+1):
+            print(start)
+            if P == start:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return dec_bin(i, k)
+            else:
+                start = start + 1
+    else:
+
+        '''se stiamo al centro'''
+        start = H - int(b/2)
+        for i in range(1,a+1):
+            n = 2**k - i
+            print(start)
+            if P == start:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return dec_bin(n,k)
+            else:
+                start = start - 1
+
+        '''se stiamo sopra'''
+        start = L
+        for l in range(0,int(b/2)):
+            print(start)
+            if P == start:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return dec_bin(l,k+1)
+            start = start + 1
+
+        '''se stiamo sotto'''
+        start = H
+        for t in range(int(b/2), b):
+            print(start)
+            if P == start:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return dec_bin(t, k+1)
+            start = start - 1
+
+def adjusted_binary_codeDec(H,L,B):
+
+    delta = H - L
+    k = math.log(delta + 1, 2)
+    potenza_di_due = k.is_integer()
+    k = int(k)
+    a = (2**(int(k)+1))-(delta + 1)
+    b = 2*(delta + 1 - 2**int(k))
+
+    if potenza_di_due == True:
+        num = int(B, base=2)
+        print(B)
+        print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+        return num + L
+    else:
+
+        '''se stiamo al centro'''
+        start = H - int(b/2)
+        for i in range(1,a+1):
+            n = 2**k - i
+            d = dec_bin(n,k)
+            print(d)
+            if B == d:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return start
+            else:
+                start = start - 1
+
+        '''se stiamo sopra'''
+        start = L
+        for l in range(0,int(b/2)):
+            d = dec_bin(l,k+1)
+            print(d)
+            if B == d:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return start
+            else:
+                start = start + 1
+
+        '''se stiamo sotto'''
+        start = H
+        for t in range(int(b/2), b):
+            d = dec_bin(t, k+1)
+            print(d)
+            if B == d:
+                print("delta = " + str(delta) + " a = " + str(a) + " b = " + str(b) + " k = " + str(k))
+                return start
+            else:
+                start = start - 1
+
+
+H = 24
+L = 17
+P = 18
+print("CODIFICA")
+B = adjusted_binary_codeCod(H,L,P)
+print(B)
+print("\nDECODIFICA")
+D = adjusted_binary_codeDec(H,L,B)
+print(D)
+
+
