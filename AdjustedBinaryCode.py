81a7f5bad87827a322400c415b0c629f961b42c3
DanieleCesarano96 <45485079+DanieleCesarano96@users.noreply.github.com>
2020-12-15 22:46:36 +0100

    Aggiunta della codifica e decodifica di AdjustedBinaryCode.py.

--- AdjustedBinaryCode.py
+++ AdjustedBinaryCode.py
@@ -4,15 +4,10 @@ def dec_bin(n,m):
 
     b = ""
     while n>0:
-
         if n%2==0:
-
             b="0"+b
-
         else:
-
             b="1"+b
-
         n=int(n/2)  #casting in intero
 
     while len(b)<m:
